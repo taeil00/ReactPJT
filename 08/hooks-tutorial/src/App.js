b54c7bcc9c9ad691c7b52375276d64b9d6d0fdbd
TAEIL JUNG <taeil00@nate.com>
2022-12-29 09:34:44 +0900

    useCallback으로 성능개선

--- 08/hooks-tutorial/src/App.js
+++ 08/hooks-tutorial/src/App.js
@@ -4,6 +4,7 @@ import Info1 from './Info1';
 import Info2 from './Info2';
 import Counter2 from './Counter2';
 import Average1 from './Average1';
+import Average2 from './Average2';
 
 const App = () => {
   // const [visible, setVisible] = useState(false);
@@ -22,7 +23,8 @@ const App = () => {
   
   //return <Counter2 />
   //return <Info2 />
-  return <Average1 />
+  // return <Average1 />
+  return <Average2 />
 };
 
 export default App;
