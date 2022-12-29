ed62563a40be6b188704d20296be7c2ed574dfee
TAEIL JUNG <taeil00@nate.com>
2022-12-29 13:57:28 +0900

    기본 레이아웃 구성

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,22 +1,28 @@
 import { useState} from 'react';
 import axios from 'axios';
 
+import NewsList from './components/NewsList';
+
 const App = () => {
   const [data, setData] = useState(null);
+  const newApiURL = "https://newsapi.org/v2/top-headlines?country=kr&apiKey=";
+  const ApiKey = 'b385cdf4f3ce41968dfaaa04b9d83a65';
+
   const onClick = async () => {
-    await axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
+    await axios.get(newApiURL + ApiKey).then(response => {
       setData(response.data);
     });
   };
 
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <div>
+  //       <button onClick={onClick}>불러오기</button>
+  //     </div>
+  //     {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
+  //   </div>
+  // );
+  return <NewsList />;
 
 };
 
